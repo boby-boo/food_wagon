2482157347b73735f7383239043ab6b6d91fc38b
Volodymyr Isyk <volodka.isyk.95@gmail.com>
2023-11-04 09:34:51 +0200

    feat @media in flash deals and rename classname section flas__cards to flash__deals

--- src/components/flashDeals/FlashDeals.js
+++ src/components/flashDeals/FlashDeals.js
@@ -9,25 +9,25 @@ import './flashDeals.scss';
 
 const FlashDeals = () => {
     return (
-        <section className='deal__cards'>
-            <div className="container">
+        <section className='flash__deals'>
+            <div className='container'>
                 <ul className='deal__cards_row'>
                     <li className='deal__card'>
                         <a href='/'>
                             <div className='deal__card_content card__content'>
-                                <div className="card__content_image">
+                                <div className='card__content_image'>
                                     <img src={dealCardImage} alt='deal card image' />
                                 </div>
                                 <div className='card__content_discount'>
                                     15
-                                    <div className="card__content_discount-items">
+                                    <div className='card__content_discount-items'>
                                         %<span>Off</span>
                                     </div>
                                 </div>
                             </div>
-                            <div className="deal__card_description">
+                            <div className='deal__card_description'>
                                 <h3>Greys Vage</h3>
-                                <div className="deal__card_description-remaining">
+                                <div className='deal__card_description-remaining'>
                                     6 Days Remaining
                                 </div>
                             </div>
@@ -36,19 +36,19 @@ const FlashDeals = () => {
                     <li className='deal__card'>
                         <a href='/'>
                             <div className='deal__card_content card__content'>
-                                <div className="card__content_image">
+                                <div className='card__content_image'>
                                     <img src={dealCardImage1} alt='deal card image' />
                                 </div>
                                 <div className='card__content_discount'>
                                     10
-                                    <div className="card__content_discount-items">
+                                    <div className='card__content_discount-items'>
                                         %<span>Off</span>
                                     </div>
                                 </div>
                             </div>
-                            <div className="deal__card_description">
+                            <div className='deal__card_description'>
                                 <h3>Greys Vage</h3>
-                                <div className="deal__card_description-remaining">
+                                <div className='deal__card_description-remaining'>
                                     6 Days Remaining
                                 </div>
                             </div>
@@ -57,19 +57,19 @@ const FlashDeals = () => {
                     <li className='deal__card'>
                         <a href='/'>
                             <div className='deal__card_content card__content'>
-                                <div className="card__content_image">
+                                <div className='card__content_image'>
                                     <img src={dealCardImage2} alt='deal card image' />
                                 </div>
                                 <div className='card__content_discount'>
                                     25
-                                    <div className="card__content_discount-items">
+                                    <div className='card__content_discount-items'>
                                         %<span>Off</span>
                                     </div>
                                 </div>
                             </div>
-                            <div className="deal__card_description">
+                            <div className='deal__card_description'>
                                 <h3>Greys Vage</h3>
-                                <div className="deal__card_description-remaining">
+                                <div className='deal__card_description-remaining'>
                                     7 Days Remaining 
                                 </div>
                             </div>
@@ -78,19 +78,19 @@ const FlashDeals = () => {
                     <li className='deal__card'>
                         <a href='/'>
                             <div className='deal__card_content card__content'>
-                                <div className="card__content_image">
+                                <div className='card__content_image'>
                                     <img src={dealCardImage3} alt='deal card image' />
                                 </div>
                                 <div className='card__content_discount'>
                                     20
-                                    <div className="card__content_discount-items">
+                                    <div className='card__content_discount-items'>
                                         %<span>Off</span>
                                     </div>
                                 </div>
                             </div>
-                            <div className="deal__card_description">
+                            <div className='deal__card_description'>
                                 <h3>Greys Vage</h3>
-                                <div className="deal__card_description-remaining">
+                                <div className='deal__card_description-remaining'>
                                     8 Days Remaining 
                                 </div>
                             </div>
