52545300fda90e900c7dd6a17bff75f95acd23d7
Volodymyr Isyk <volodka.isyk.95@gmail.com>
2023-12-30 15:32:56 +0200

    fix: removed scrollbar when modal is open

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -47,11 +47,31 @@ const Header = () => {
     const handleNavigate = () => {
         if (location.pathname.search(/search/) === -1) navigate('/search/all')
     }
+
+    const calcScroll = () => {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+
+    const scrollWidth = calcScroll();
     
     if (isOpenModalWindow) {
-        document.body.style.overflow = 'hidden'
+        console.log(scrollWidth)
+        document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scrollWidth}px`;
     } else {
-        document.body.style.overflow = 'visible'
+        document.body.style.overflow = 'visible';
+        document.body.style.marginRight = `0px`;
     }
 
     const border = {
