0d62ea1c770405e02398e29a63c38361eb3e5dc2
Volodymyr Isyk <volodka.isyk.95@gmail.com>
2024-01-09 20:26:06 +0200

    refactor: all content in AboutService added in an array and rendered through method map

--- src/components/aboutService/AboutService.js
+++ src/components/aboutService/AboutService.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-import img1 from '../../resources/icons/about__service/about__service_icon-1.svg';
-import img2 from '../../resources/icons/about__service/about__service_icon-2.svg';
-import img3 from '../../resources/icons/about__service/about__service_icon-3.svg';
-import img4 from '../../resources/icons/about__service/about__service_icon-4.svg';
-
 import './aboutService.scss';
 
 const OurService = () => {
+    const items = [
+        {
+            title: 'Select location',
+            description:
+                'Choose the location where your food will be delivered.',
+        },
+        {
+            title: 'Choose order',
+            description:
+                'Check over hundreds of menus to pick your favorite food',
+        },
+        {
+            title: 'Pay advanced',
+            description:
+                'It`s quick, safe, and simple. Select several methods of payment',
+        },
+        {
+            title: 'Enjoy meals',
+            description: 'Food is made and delivered directly to your home.',
+        },
+    ];
     return (
         <motion.section
             className="about-service"
@@ -19,54 +35,28 @@ const OurService = () => {
             <div className="container">
                 <h2 className="primary-title">How does it work</h2>
                 <ul className="about-service__row">
-                    <motion.li className="about-service__row_item row__item">
-                        <div className="row__item_image">
-                            <img src={img1} alt="Select location icon" />
-                        </div>
-                        <div className="row__item_text">
-                            <h3>Select location</h3>
-                            <p>
-                                Choose the location where your food will be
-                                delivered.
-                            </p>
-                        </div>
-                    </motion.li>
-                    <li className="about-service__row_item row__item">
-                        <div className="row__item_image">
-                            <img src={img2} alt="Choose order icon" />
-                        </div>
-                        <div className="row__item_text">
-                            <h3>Choose order</h3>
-                            <p>
-                                Check over hundreds of menus to pick your
-                                favorite food
-                            </p>
-                        </div>
-                    </li>
-                    <li className="about-service__row_item row__item">
-                        <div className="row__item_image">
-                            <img src={img3} alt="Pay advanced icon" />
-                        </div>
-                        <div className="row__item_text">
-                            <h3>Pay advanced</h3>
-                            <p>
-                                It`&apos;`s quick, safe, and simple. Select
-                                several methods of payment
-                            </p>
-                        </div>
-                    </li>
-                    <li className="about-service__row_item row__item">
-                        <div className="row__item_image">
-                            <img src={img4} alt="Enjoy meals icon" />
-                        </div>
-                        <div className="row__item_text">
-                            <h3>Enjoy meals</h3>
-                            <p>
-                                Food is made and delivered directly to your
-                                home.
-                            </p>
-                        </div>
-                    </li>
+                    {items.map((item, index) => {
+                        const { title, description } = item,
+                            image = require(
+                                `../../resources/icons/about__service/about__service_icon-${
+                                    index + 1
+                                }.svg`,
+                            );
+                        return (
+                            <li
+                                key={index}
+                                className="about-service__row_item row__item"
+                            >
+                                <div className="row__item_image">
+                                    <img src={image} alt="Choose order icon" />
+                                </div>
+                                <div className="row__item_text">
+                                    <h3>{title}</h3>
+                                    <p>{description}</p>
+                                </div>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
         </motion.section>
